0aaabc408c181887cd58391ebcb7512efe4dc3e5
Diego Alvarez <dendrit83@protonmail.ch>
2021-03-28 16:30:09 -0300

    uploading first files

---
+++ data-pagination-and-filtering-v1/js/script.js
@@ -0,0 +1,30 @@
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+*/
+
+
+
+/*
+For assistance:
+   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
+   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+*/
+
+
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+
+
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+
+
+// Call functions
