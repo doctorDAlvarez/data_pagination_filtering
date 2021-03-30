2373168123a26a5127e930f1711eb7708be5924b
Diego Alvarez <dendrit83@protonmail.ch>
2021-03-30 19:46:11 -0300

    fix looping bug

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -1,48 +1,43 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
+/********************************************************
+[*] Treehouse Techdegree:
+[*] FSJS Project 2 - Data Pagination and Filtering
+[*] by Diego Alvarez. @doc on slack.
+[*] March 2021.
+*********************************************************
 */
-
-
-
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
 /*
 Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+This function will create and insert/append the elements needed to display
+a "page" of nine students
 */
+
 function showPage( list, page ) {
-  let startIndex = (page * 8) - 8;
-  let endIndex = page * 8;
+  let startIndex = (page * 9) - 9;
+  let endIndex = page * 9;
   const ul = document.querySelector(".student-list");
-  ul.innerHTML = "";
-  for (let i = startIndex; i <= endIndex; i++ ) {
-    ul.insertAdjacentHTML( "beforeend",
-    ` <li class="student-item cf">
-        <div class="student-details">
-          <img class="avatar" src="${list[i].picture.thumbnail}">
-          <h3>${list[i].name.first} ${list[i].name.last}</h3>
-          <span class="email">${list[i].email}</span>
-        </div>
-        <div class="joined-details">
-          <span class="date">Joined ${list[i].registered.date}</span>
-        </div>
-      </li>
-      `);
+  ul.innerHTML = ""; //refreshing the page.
+  for (let i = 0; i < list.length; i++ ) {
+    if ( i >= startIndex && i < endIndex) {
+      ul.insertAdjacentHTML( "beforeend", //template literal for the HTML to add.
+      ` <li class="student-item cf">
+            <div class="student-details">
+                <img class="avatar" src="${list[i].picture.thumbnail}">
+                <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+              <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+        </li>
+        `);
+      }
   }
 }
-
-
-
 /*
 Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+This function will create and insert/append the elements needed
+for the pagination buttons
 */
 function addPagination( list ) {
   let paginationButtons = Math.ceil((list.length / 9));
@@ -63,14 +58,17 @@ function addPagination( list ) {
       const active_button = ul.querySelector(".active");
       active_button.className = "";
       e.target.className = "active";
+
       showPage(data, e.target.textContent);
     }
-  })
+  });
 }
-
-
 
 // Call functions
 
 showPage(data , 1);
 addPagination(data);
+
+// theEnd.
+// have a nice coding day! ;) @doc
+// ----------------------------------------------------------------------------
