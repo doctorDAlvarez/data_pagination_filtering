5c3470df243b45351aa79aa6271bbd0a80344b7c
Diego Alvarez <dendrit83@protonmail.ch>
2021-03-30 23:18:07 -0300

    exceeds update and comments

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# data_pagination_filtering
- techDegree_Project 2 
+## Data Pagination and Filtering
+Second project of my **techDegree**. This project uses a student database in the format of an
+array of objects datastructure, and implements **paging**, **filtering** in a cool search feature.
+
+## Build status
+Up and working.
+
+
+## Features
+Paging and filtering databases.
+
+## Installation
+
+Just clone the repo or download the files and open index.html.
+## API Reference
+uses MATH built-in JS.
+
+
+## Credits
+TeamTreeHouse Full Stack JavaScript techDegree.
+HTML & CSS property of TeamTreeHouse.
+
+
+MIT © [doc]()
--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -6,69 +6,110 @@
 *********************************************************
 */
 /*
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display
-a "page" of nine students
-*/
+ * Selecting the elements of the DOM that we plan working with.
+ * The header for the search feature.
+ * The ul for the list of students.
+ * The div to insert our custom p tag to track students found.
+ */
+const ul = document.querySelector(".student-list");
+const studentsFound = document.createElement("p");
+const header = document.querySelector(".header");
+const div = document.querySelector(".page");
+div.insertBefore(studentsFound, ul);
+// create the template literal for the HTML new search feature.
+let html_search = `
+  <label for="search" class="student-search">
+    <span>Search by name</span>
+    <input id="search" placeholder="Search by name...">
+    <button type="button"><img src="img/icn-search.svg" alt="Search icon">
+      </button>
+  </label>`;
+// adding the new search feature to the header.
+header.insertAdjacentHTML("beforeend", html_search);
 
-function showPage( list, page ) {
-  let startIndex = (page * 9) - 9;
-  let endIndex = page * 9;
-  const ul = document.querySelector(".student-list");
-  ul.innerHTML = ""; //refreshing the page.
-  for (let i = 0; i < list.length; i++ ) {
-    if ( i >= startIndex && i < endIndex) {
-      ul.insertAdjacentHTML( "beforeend", //template literal for the HTML to add.
-      ` <li class="student-item cf">
+/**
+ * Function: showPage()
+ * Principal function to update the page with the matched students.
+ * @param {array of objects} list - The array of students to work with.
+ * @param {Number} page - The integer number of the page to show.
+ **/
+function showPage(list, page) {
+	let startIndex = (page * 9) - 9;
+	let endIndex = page * 9;
+	ul.innerHTML = ""; //refresh the ul tag.
+	studentsFound.innerHTML = "";
+	for (let i = 0; i < list.length; i++) {
+		if (i >= startIndex && i < endIndex) {
+			ul.insertAdjacentHTML("beforeend", //template literal for the <li> to add.
+				`
+        <li class="student-item cf">
             <div class="student-details">
-                <img class="avatar" src="${list[i].picture.thumbnail}">
-                <h3>${list[i].name.first} ${list[i].name.last}</h3>
-                <span class="email">${list[i].email}</span>
+                <img class="avatar" src=" ${list[i].picture.thumbnail} ">
+                <h3> ${list[i].name.first} ${list[i].name.last} </h3>
+                <span class="email"> ${list[i].email} </span>
             </div>
             <div class="joined-details">
-              <span class="date">Joined ${list[i].registered.date}</span>
+              <span class="date"> Joined ${list[i].registered.date} </span>
             </div>
         </li>
         `);
-      }
-  }
+		}
+	}
+  // - testing the search result, to choose the message to print.
+  if (list.length !== 0) {
+		studentsFound.innerHTML = `${list.length} students found.`;
+	} else {
+		studentsFound.innerHTML = "No results found";
+	}
+	studentsFound.style.margin = "6px";
 }
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed
-for the pagination buttons
-*/
-function addPagination( list ) {
-  let paginationButtons = Math.ceil((list.length / 9));
-  // console.log(paginationButtons);
-  const ul = document.querySelector(".link-list");
-  ul.innerHTML = "";
-  for(let i = 1; i <= paginationButtons; i++) {
-    ul.insertAdjacentHTML("beforeend",
-    `
+/**
+* Function: addPagination()
+* Secondary function that adds the functionality for the paging buttons.
+* @param {array of objects} list - The array of students to work with.
+**/
+function addPagination(list) {
+  //first we calculate the Nº of pages that we need.
+	const paginationButtons = Math.ceil((list.length / 9));
+	const ul = document.querySelector(".link-list");
+	ul.innerHTML = "";
+	for (let i = 1; i <= paginationButtons; i++) {     //adding the buttons
+		ul.insertAdjacentHTML("beforeend", `
       <li>
         <button type="button">${i}</button>
       </li>
     `);
-  }
-  ul.firstElementChild.className = "active";
-  ul.addEventListener("click", function (e) {
-    if (e.target.tagName === "BUTTON") {
-      const active_button = ul.querySelector(".active");
-      active_button.className = "";
-      e.target.className = "active";
-
-      showPage(data, e.target.textContent);
-    }
-  });
+	}
+  if (ul.firstElementChild) {
+		ul.firstElementChild.className = "active";
+	}
+	ul.addEventListener("click", function(e) {         //adding paging handler
+		if (e.target.tagName === "BUTTON") {
+			const active_button = ul.querySelector(".active");
+			active_button.className = "";
+			e.target.className = "active";
+			showPage(list, e.target.textContent);
+		}
+	});
 }
-
-// Call functions
-
-showPage(data , 1);
+const inputElem = document.getElementById("search"); //search feature handler
+inputElem.addEventListener("input", (e) => {
+	let new_page = [];
+	for (let i = 0; i < data.length; i++) {
+		firstName = data[i].name.first.toUpperCase();
+		lastName = data[i].name.last.toUpperCase();
+		inputValue = inputElem.value.toUpperCase();
+		if (firstName.includes(inputValue) || lastName.includes(inputValue)) {
+			new_page.push(data[i]);
+		}
+	}
+	ul.innerHTML = "";
+	showPage(new_page, 1);
+	addPagination(new_page);
+});
+// Call functions to display the initial page of all students on the list.
+showPage(data, 1);
 addPagination(data);
-
 // theEnd.
 // have a nice coding day! ;) @doc
 // ----------------------------------------------------------------------------
